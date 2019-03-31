ff5b0e93849efc4227a6e01be522b16f158b3c8f
thaining7 <thaining@protonmail.com>
2019-03-31 16:13:17 -0500

    updated liri

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var fs = require("fs");
 var request = require("request");
 var moment = require("moment");
 moment().format();
@@ -23,16 +24,26 @@ if (liriInput === "movie-this") {
         function (response) {
 
             // console.log("The movie's rating is: " + response.data.imdbRating);
+            // console.log(response.data);
             console.log(JSON.stringify(response.data, null, 2));
         });
-}
 
-if (liriInput === "spotify-this-song") {
+}
+// else (liriInput === "movie-this") {
+//     axios.get("http://www.omdbapi.com/?t=Mr. Nobody&y=&plot=short&apikey=trilogy").then(
+//         function (response) {
+
+//             // console.log("The movie's rating is: " + response.data.imdbRating);
+//             console.log(JSON.stringify(response.data, null, 2));
+//         });
+// }
+
+else if (liriInput === "spotify-this-song") {
     for (i = 3; i < liriInput1.length; i++) {
         songTitle += liriInput1[i] + "";
     }
     spotify
-        .search({ type: 'track', query: songTitle})
+        .search({ type: 'track', query: songTitle })
         .then(function (response) {
             console.log(response);
         })
@@ -41,11 +52,15 @@ if (liriInput === "spotify-this-song") {
         });
 }
 
-if (liriInput === "concert-this") {
+else if (liriInput === "concert-this") {
     for (i = 3; i < liriInput1.length; i++) {
         artistName += liriInput1[i] + "";
     }
-        axios.get("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp").then( function(response) {
-            console.log(JSON.stringify(response.data, null, 2));
-        });
+    axios.get("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp").then(function (response) {
+        console.log(JSON.stringify(response.data, null, 2));
+    });
+}
+
+else {
+    console.log('Please type one of the following commands: concert-this "band name", movie-this "name of movie", spotify-this-song "name of song"');
 }
